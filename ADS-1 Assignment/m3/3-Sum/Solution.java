467859693d415baffc11bea0949d8a75d2362adc
nandakishore723 <nandakishore723@msitprogram.net>
2018-09-27 16:07:43 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

---
+++ ADS-1 Assignment/m3/3-Sum/Solution.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+import java.util.Arrays;
+/**.
+ * Class for solution.
+ */
+public final class Solution {
+    /**.
+     * { function_description }
+     *
+     * @param      args  The arguments
+     */
+    /**
+         * Constructs the object.
+         */
+        private Solution() {
+
+        }
+        /**.
+         * { function_description }
+         *
+         * @param      args  The arguments
+         */
+    public static void main(final String[] args) {
+        Scanner ip = new Scanner(System.in);
+        int k, n, cnt = 0;
+        n = ip.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = ip.nextInt();
+        }
+        
+        Arrays.sort(arr);
+        for (int i = 0; i < n - 2; i++) {
+            int j = i + 1;
+            int x = n - 1;
+            while (j < x) {
+                if (arr[i] + arr[j] + arr[x] == 0) {
+                    cnt++;
+                    j++;
+                    x--;
+
+                } else if (arr[i] + arr[j] + arr[x] < 0) {
+                    j++;
+
+                } else {
+                    x--;
+                }
+            }
+
+        }
+        System.out.println(cnt);
+    }
+}
