42fd12f5ab8e4904f6ae9ea8ea267866621138dc
nandakishore723 <nandakishore723@msitprogram.net>
2018-09-30 20:05:56 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

---
+++ ADS-1 Assignment/m4/Solution.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+/**
+ * Class for solution.
+ */
+final class Solution {
+
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+
+    
+    }
+    /**
+     * Checks if Parenthesis is balanced.
+     *
+     * @param      str     String.
+     *
+     * @return     Trueif balanced, Otherwise False.
+     */
+    public static boolean balancedParenthensies(final String str) {
+        MyStack<Character> stack = new MyStack<>();
+        for (int i = 0; i < str.length(); i++) {
+            Character c = str.charAt(i);
+            if (c == '[' || c == '(' || c == '{') {
+                stack.push(c);
+            } else if (c == ']') {
+                if (stack.isEmpty() || stack.pop() != '[') {
+                    return false;
+                }
+            } else if (c == ')') {
+                if (stack.isEmpty() || stack.pop() != '(') {
+                    return false;
+                }
+            } else if (c == '}') {
+                if (stack.isEmpty() || stack.pop() != '{') {
+                    return false;
+                }
+            }
+
+        }
+        return stack.isEmpty();
+    }
+
+    /**
+     * Main method.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
+        while (scan.hasNext()) {
+            if (balancedParenthensies(scan.next())) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+        }
+    }
+}
