9827a2e87826172191ca3100d914092c244e669f
nandakishore723 <nandakishore723@msitprogram.net>
2018-10-07 16:17:57 +0530

    "ads1_m5_a1: 5/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS-1 Assignment/m5/5.1 Deque/MyQueue.java
+++ ADS-1 Assignment/m5/5.1 Deque/MyQueue.java
@@ -20,8 +20,8 @@ public class MyQueue<E> {
 		node.data = data;
 		node.next = head;
 		if (head == null) {
-			head=node;
-			tail=head;
+			head = node;
+			tail = head;
 			size++;
 			return;
 		}
