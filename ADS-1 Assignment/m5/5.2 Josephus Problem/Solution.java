7c9e9bcd47a3320c44bd93a46e266cc872b06e92
nandakishore723 <nandakishore723@msitprogram.net>
2018-10-07 16:32:26 +0530

    "ads1_m5_a2: 5/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-1 Assignment/m5/5.2 Josephus Problem/Solution.java
+++ ADS-1 Assignment/m5/5.2 Josephus Problem/Solution.java
@@ -2,9 +2,9 @@ import java.util.*;
 public class Solution {
 	public static void main(String[] args) {
 		Scanner ip = new Scanner(System.in);
-		String testcases =ip.nextLine();
+		String testcases = ip.nextLine();
 		while (ip.hasNext()) {
-			String[] line =ip.nextLine().split(" ");
+			String[] line = ip.nextLine().split(" ");
 			int people = Integer.parseInt(line[0]);
 			int gap = Integer.parseInt(line[1]);
 			Deque<Integer> q = new Deque<>();
