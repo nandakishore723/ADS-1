84278465e5929fbf4282a832aaec450b2d8c0b18
nandakishore723 <nandakishore723@msitprogram.net>
2018-10-07 18:12:06 +0530

    "ads1_m8_a2: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ ADS-1 Assignment/m8/8.2 Team Ranking With Selection/Solution.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+final class Solution {
+    /**
+     * Solution class for Selection sort program.
+     *
+     */
+
+    private Solution() {
+        /**
+         * { item_description }.
+         */
+    }
+
+    /**
+     * Main function to take inputs.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(String[] args) {
+        /**
+         * In main method the given input is split into tokens and
+         * these tokens are passed as parameters to the selection
+         * sort class.
+         */
+        final int three = 3;
+        Scanner sc=new Scanner(System.in);
+        StringBuffer sb=new StringBuffer();
+        while (sc.hasNext()) {
+            sb.append(sc.nextLine() + "::");
+            // System.out.println(sb);
+        }
+        String[] lines=sb.toString().split("::");
+        // System.out.println(sb.toString().split("::"));
+        int i = 0;
+        Team[] teams = new Team[lines.length];
+        for (String line : lines) {
+            String[] tokens=line.split(",");
+            Team team =new Team(tokens[0], Integer.parseInt(tokens[1]),
+                Integer.parseInt(tokens[2]), Integer.parseInt(tokens[three]));
+            teams[i++] = team;
+        }
+        Selection.sort(teams);
+        String output = "";
+        for (Team team : teams) {
+            output +=team +",";
+        }
+        System.out.print(output.substring(0, output.length() - 1));
+    }
+}
