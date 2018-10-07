c22db3bfd2a7ed62df804cacda1d4107d15c5a86
nandakishore723 <nandakishore723@msitprogram.net>
2018-10-07 16:57:55 +0530

    "ads1_m5_a2: 5/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-1 Assignment/m5/5.2 Josephus Problem/Solution.java
+++ ADS-1 Assignment/m5/5.2 Josephus Problem/Solution.java
@@ -29,7 +29,8 @@ public final class Solution {
 	 * @param      q       The quarter
 	 * @param      people  The people
 	 */
-	public static void createCircle(final Deque<Integer> q, final int people) {
+	public static void createCircle(final Deque<Integer> q,
+	 final int people) {
 		for (int i = 0; i < people; i++) {
 			q.push(i);
 		}
