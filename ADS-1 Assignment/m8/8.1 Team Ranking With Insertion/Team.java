d2ab752f6bf4f44bee07ec7b84c77e6142f24c7d
nandakishore723 <nandakishore723@msitprogram.net>
2018-10-25 00:25:51 +0530

    "ads1_m8_a1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-1 Assignment/m8/8.1 Team Ranking With Insertion/Team.java
+++ ADS-1 Assignment/m8/8.1 Team Ranking With Insertion/Team.java
@@ -2,141 +2,141 @@
  * Class for team.
  */
 class Team implements Comparable<Team> {
-	/**The Team class also implements the interface
-	of the Comparable class.**/
-	private String country;
-	/**
-	 * { var_description }.
-	 */
-	private int wins;
-	/**
-	 * { var_description }.
-	 */
-	private int loses;
-	/**
-	 * { var_description }.
-	 */
-	private int draws;
-	/**
-	 * Constructs the object.
-	 *
-	 * @param      country1  The countryT
-	 * @param      wins1     The wins
-	 * @param      loses1    The loses
-	 * @param      draws1    The draws
-	 */
-	Team(final String country1, final int wins1, final int loses1,
-	     final int draws1) {
-		this.country = country1;
-		this.wins = wins1;
-		this.loses = loses1;
-		this.draws = draws1;
-	}
-	/**
-	 * Gets country.
-	 *
-	 * @return     country.
-	 */
-	public String getcountry() {
-		return country;
-	}
-	/**
-	 * Sets country.
-	 *
-	 * @param      country  The country.
-	 */
-	public void setcountry(final String country1) {
-		this.country = country1;
-	}
-	/**
-	 * Get wins.
-	 *
-	 * @return     wins.
-	 */
-	public int getwins() {
-		return wins;
-	}
-	/**
-	 * Set wins.
-	 *
-	 * @param      wins  The wins.
-	 */
-	public void setwins(final int wins1) {
-		this.wins = wins1;
-	}
-	/**
-	 * Get loses.
-	 *
-	 * @return     loses.
-	 */
-	public int getloses() {
-		return loses;
-	}
-	/**
-	 * Set loses.
-	 *
-	 * @param      loses  The loses.
-	 */
-	public void setloses(final int loses1) {
-		this.loses = loses1;
-	}
-	/**
-	 * Get draws.
-	 *
-	 * @return     draws.
-	 */
-	public int getdraws() {
-		return draws;
-	}
-	/**
-	 * Set draws.
-	 *
-	 * @param      draws  The draws.
-	 */
-	public void setdraws(final int draws1) {
-		this.draws = draws1;
-	}
-	/**
-	 * Returns a string representation of the object.
-	 *
-	 * @return     String representation of the object.
-	 */
-	public String toString() {
-		return this.country;
-	}
-	/**
-	 * comapares two objects in the given condition.
-	 *
-	 * @param      that  The that
-	 *
-	 * @return
-	 */
-	public int compareTo(final Team that) {
-		Team a = this;
-		if (a.wins > that.wins) {
+    /**The Team class also implements the interface
+    of the Comparable class.**/
+    private String country;
+    /**
+     * { var_description }.
+     */
+    private int wins;
+    /**
+     * { var_description }.
+     */
+    private int loses;
+    /**
+     * { var_description }.
+     */
+    private int draws;
+    /**
+     * Constructs the object.
+     *
+     * @param      country1  The countryT
+     * @param      wins1     The wins
+     * @param      loses1    The loses
+     * @param      draws1    The draws
+     */
+    Team(final String country1, final int wins1, final int loses1,
+         final int draws1) {
+        this.country = country1;
+        this.wins = wins1;
+        this.loses = loses1;
+        this.draws = draws1;
+    }
+    /**
+     * Gets country.
+     *
+     * @return     country.
+     */
+    public String getcountry() {
+        return country;
+    }
+    /**
+     * Sets country.
+     *
+     * @param      country  The country.
+     */
+    public void setcountry(final String country1) {
+        this.country = country1;
+    }
+    /**
+     * Get wins.
+     *
+     * @return     wins.
+     */
+    public int getwins() {
+        return wins;
+    }
+    /**
+     * Set wins.
+     *
+     * @param      wins  The wins.
+     */
+    public void setwins(final int wins1) {
+        this.wins = wins1;
+    }
+    /**
+     * Get loses.
+     *
+     * @return     loses.
+     */
+    public int getloses() {
+        return loses;
+    }
+    /**
+     * Set loses.
+     *
+     * @param      loses  The loses.
+     */
+    public void setloses(final int loses1) {
+        this.loses = loses1;
+    }
+    /**
+     * Get draws.
+     *
+     * @return     draws.
+     */
+    public int getdraws() {
+        return draws;
+    }
+    /**
+     * Set draws.
+     *
+     * @param      draws1  The draws.
+     */
+    public void setdraws(final int draws1) {
+        this.draws = draws1;
+    }
+    /**
+     * Returns a string representation of the object.
+     *
+     * @return     String representation of the object.
+     */
+    public String toString() {
+        return this.country;
+    }
+    /**
+     * comapares two objects in the given condition.
+     *
+     * @param      that  The that
+     *
+     * @return
+     */
+    public int compareTo(final Team that) {
+        Team a = this;
+        if (a.wins > that.wins) {
 
-			return 1;
-		}
-		if (a.wins < that.wins) {
+            return 1;
+        }
+        if (a.wins < that.wins) {
 
-			return -1;
-		}
-		if (a.loses > that.loses) {
+            return -1;
+        }
+        if (a.loses > that.loses) {
 
-			return -1;
-		}
-		if (a.loses < that.loses) {
+            return -1;
+        }
+        if (a.loses < that.loses) {
 
-			return 1;
-		}
-		if (a.draws > that.draws) {
+            return 1;
+        }
+        if (a.draws > that.draws) {
 
-			return 1;
-		}
-		if (a.draws < that.draws) {
+            return 1;
+        }
+        if (a.draws < that.draws) {
 
-			return -1;
-		}
-		return 0;
-	}
+            return -1;
+        }
+        return 0;
+    }
 }
