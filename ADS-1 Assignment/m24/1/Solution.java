4734f735d008183b84a93fc03eb7767db4c5da7b
nandakishore723 <nandakishore723@msitprogram.net>
2018-10-27 12:13:00 +0530

    "m24_a1_1: 0/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-1 Assignment/m24/1/Solution.java
+++ ADS-1 Assignment/m24/1/Solution.java
@@ -19,29 +19,35 @@ class Solution {
 	 */
 	public static void main(String[] args) {
 		Scanner ip = new Scanner(System.in);
-		int n =Integer.parseInt(ip.nextLine());
-		SeparateChainingHashST<Integer, Student> schObj= new SeparateChainingHashST<>();
+		int n = Integer.parseInt(ip.nextLine());
+		SeparateChainingHashST<Integer, Student> schObj =
+		    new SeparateChainingHashST<>();
 		int i;
-		for (i = 0; i < n; i++){
+		for (i = 0; i < n; i++) {
 			String[] tokens = ip.nextLine().split(",");
-			Student stdObj = new Student(tokens[1],Double.parseDouble(tokens[2]));
+			Student stdObj = new Student(tokens[1],
+			                             Double.parseDouble(tokens[2]));
 			schObj.put(Integer.parseInt(tokens[0]), stdObj);
 		}
 		int m = Integer.parseInt(ip.nextLine());
 		int j;
-		for (j = 0; j <m; j++){
+		for (j = 0; j < m; j++) {
 			String[] queryTokens = ip.nextLine().split(" ");
 			if (queryTokens[0].equals("get")) {
 				if (Integer.parseInt(queryTokens[2]) == 1) {
-					if (schObj.get(Integer.parseInt(queryTokens[1])) == null) {
+					if (schObj.get(Integer.parseInt(
+					                   queryTokens[1])) == null) {
 						System.out.println("Student doesn't exists...");
 					} else {
-						System.out.println(schObj.get(Integer.parseInt(queryTokens[1])).getstudentname());
+						System.out.println(schObj.get(Integer.parseInt(
+						                                  queryTokens[1])).getstudentname());
 
 						if (schObj.get(Integer.parseInt(queryTokens[1])) == null) {
 							System.out.println("Student doesn't exists...");
 						} else {
-							System.out.println(schObj.get(Integer.parseInt(queryTokens[1])).gettotalmarks());
+							System.out.println(schObj.get(
+							                       Integer.parseInt(
+							                           queryTokens[1])).gettotalmarks());
 						}
 					}
 				}
