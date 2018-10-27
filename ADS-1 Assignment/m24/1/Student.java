bd71ede69044bfe5d183c6e073bfa3ef62f9eff7
nandakishore723 <nandakishore723@msitprogram.net>
2018-10-27 12:14:58 +0530

    "m24_a1_1: 0/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-1 Assignment/m24/1/Student.java
+++ ADS-1 Assignment/m24/1/Student.java
@@ -2,63 +2,63 @@
  * Class for student.
  */
 class Student {
-	private String studentname;
-	private double totalmarks;
+    private String studentname;
+    private double totalmarks;
 
-	/**
-	 * Constructs the object.
-	 *
-	 * @param      studentname  The studentname
-	 * @param      totalmarks   The totalmarks
-	 */
-	Student(String studentname, double totalmarks) {
-		this.studentname = studentname;
-		this.totalmarks = totalmarks;
-	}
+    /**
+     * Constructs the object.
+     *
+     * @param      studentname  The studentname
+     * @param      totalmarks   The totalmarks
+     */
+    Student(String studentname, double totalmarks) {
+        this.studentname = studentname;
+        this.totalmarks = totalmarks;
+    }
 
-	/**
-	 * Gets the studentname.
-	 *
-	 * @return     The studentname.
-	 */
-	public String getstudentname() {
-		return studentname;
-	}
+    /**
+     * Gets the studentname.
+     *
+     * @return     The studentname.
+     */
+    public String getstudentname() {
+        return studentname;
+    }
 
-	/**
-	 * Sets the studentname.
-	 *
-	 * @param      studentname  The studentname
-	 */
-	public void setstudentname(String studentname) {
-		this.studentname = studentname;
-	}
+    /**
+     * Sets the studentname.
+     *
+     * @param      studentname  The studentname
+     */
+    public void setstudentname(String studentname) {
+        this.studentname = studentname;
+    }
 
-	/**
-	 * Gets the totalmarks.
-	 *
-	 * @return     The totalmarks.
-	 */
-	public double gettotalmarks() {
-		return totalmarks;
-	}
+    /**
+     * Gets the totalmarks.
+     *
+     * @return     The totalmarks.
+     */
+    public double gettotalmarks() {
+        return totalmarks;
+    }
 
-	/**
-	 * sets the totalmarks.
-	 *
-	 * @param      totalmarks  The totalmarks
-	 */
-	public void settotalmarks(double totalmarks) {
-		this.totalmarks = totalmarks;
-	}
-	/**
-	 * Returns a string representation of the object.
-	 *
-	 * @return     String representation of the object.
-	 */
-	public String toString() {
-		String str = "";
-		str = str + this.studentname + this.totalmarks;
-		return str;
-	}
+    /**
+     * sets the totalmarks.
+     *
+     * @param      totalmarks  The totalmarks
+     */
+    public void settotalmarks(double totalmarks) {
+        this.totalmarks = totalmarks;
+    }
+    /**
+     * Returns a string representation of the object.
+     *
+     * @return     String representation of the object.
+     */
+    public String toString() {
+        String str = "";
+        str = str + this.studentname + this.totalmarks;
+        return str;
+    }
 }
